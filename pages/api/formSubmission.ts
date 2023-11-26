3a00b52e4d2a6fd737e055824730c3bd66038ac3
Nouman Ahmad <noumana@plcgroup.com>
2023-11-26 16:58:08 +1100

    changes in gitignore

---
+++ pages/api/formSubmission.ts
@@ -0,0 +1,42 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import prisma from '../../prisma/client/index';
+
+function generateSerialNumber() {
+   const characters = 'asdf123234';
+   const serialNumberLength = 8;
+ 
+   let serialNumber = '';
+   for (let i = 0; i < serialNumberLength; i++) {
+     const randomIndex = Math.floor(Math.random() * characters.length);
+     serialNumber += characters.charAt(randomIndex);
+   }
+ 
+   return serialNumber;
+}
+ 
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+
+  if (req.method === 'POST') {
+    try {
+      const { model, licenseLevel, quantity, date } = req.body;
+
+      const result = await prisma.batch.create({
+        data: {
+          model,
+          licenseLevel,
+          quantity,
+          date,
+          serialNumber: generateSerialNumber()
+        },
+      });
+
+      res.status(200).json({ success: true, data: result });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ success: false, error: 'Internal Server Error' });
+    }
+  } else {
+    res.status(405).json({ success: false, error: 'Method Not Allowed' });
+  }
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,118 +1,128 @@
+// pages/Form.tsx
+import React, { useState } from 'react';
 import Image from 'next/image'
-import { Inter } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+interface FormValues {
+  model: string;
+  licenseLevel: number;
+  quantity: number;
+  date: string;
+  comments: string;
+}
 
-export default function Home() {
+const Form: React.FC = () => {
+  const [formData, setFormData] = useState<FormValues>({
+    model: '',
+    licenseLevel: 0,
+    quantity: 0,
+    date: '',
+    comments: '',
+  });
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({ ...prevData, [name]: name === 'quantity' ? parseInt(value, 10) : value }));
+  };
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    try {
+      const response = await fetch('api/formSubmission', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+  
+      if (response.ok) {
+        const data = await response.json();
+        console.log('Form submitted successfully:', data);
+      } else {
+        console.error('Failed to submit form');
+      }
+    } catch (error) {
+      console.error('Error submitting form:', error);
+    }
+  };
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+    <div className="min-h-screen flex bg-black">
+
+      <Image src="fcllogo.svg" alt="Logo" className="h-16 w-16 m-4"  width={800} height={500} />
+
+      <div className="flex items-center justify-center flex-grow">
+        <div className="max-w-md w-full bg-black border-2 border-yellow-700 p-8 rounded-sm shadow-md">
+          <h1 className='text-white'>Batch Form</h1>
+          <form onSubmit={handleSubmit}>
+            <select
+              id="model"
+              name="model"
+              value={formData.model}
+              onChange={handleInputChange}
+              className="my-3 p-1 block w-full  rounded-sm sm:text-sm text-black"
+              placeholder="Model"
+            >
+              <option value="Model 1">Model 1</option>
+              <option value="Model 2">Model 2</option>
+              <option value="Model 3">Model 3</option>
+            </select>
+
+            <input
+              type="date"
+              id="date"
+              name="date"
+              value={formData.date}
+              onChange={handleInputChange}
+              className="my-3 p-1 block w-full  rounded-sm sm:text-sm "
+              placeholder="Date"
             />
-          </a>
+
+            <input
+              type="number"
+              id="quantity"
+              name="quantity"
+              value={formData.quantity}
+              onChange={handleInputChange}
+              className="my-3 p-1 block w-full  rounded-sm sm:text-sm "
+              placeholder="Quantity"
+            />
+
+            <select
+              id="licenseLevel"
+              name="licenseLevel"
+              value={formData.licenseLevel}
+              onChange={handleInputChange}
+              className="my-3 p-1 block w-full  rounded-sm sm:text-sm "
+              placeholder="License Level"
+            >
+              {[...Array(10).fill(null).keys()].map((level) => (
+                <option key={level} value={level}>
+                  {level}
+                </option>
+              ))}
+            </select>
+
+            <textarea
+              id="comments"
+              name="comments"
+              value={formData.comments}
+              disabled
+              className="my-3 p-1 block w-full  rounded-sm sm:text-sm "
+              placeholder="Comments"
+              rows={2}
+            ></textarea>
+
+            <button
+              type="submit"
+              className="w-full bg-yellow-700  p-2 rounded hover:bg-yellow-600 text-white"
+            >
+              Submit
+            </button>
+          </form>
         </div>
       </div>
+    </div>
+  );
+};
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
-}
+export default Form;
